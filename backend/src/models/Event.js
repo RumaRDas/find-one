6afe72ed08e03c7ebf247f9b8ed20f2c6656013b
Ruma Das <rumaadl16@gmail.com>
2020-07-24 00:52:40 +0930

    adding user id in event model

--- backend/src/models/Event.js
+++ backend/src/models/Event.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const EventSchema = new mongoose.Schema({
     title: String,
     description: String,
-    price: Number,
-    thumbnail: String
+    cost: Number,
+    categories: String,
+    thumbnail: String,
+    date: Date,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
 })
 const Event = mongoose.model('Event', EventSchema);
 module.exports = Event;

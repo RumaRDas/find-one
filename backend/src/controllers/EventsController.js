cd37983bcdf99379ae1518e620337ca106548803
Ruma Das <rumaadl16@gmail.com>
2020-07-24 01:07:47 +0930

    adding create event function

--- backend/src/controllers/EventsController.js
+++ backend/src/controllers/EventsController.js
@@ -1,5 +1,30 @@
 const db = require('../models');
 
 module.exports ={
+    async createEvent(req, res) {
+        try {
+            const { title, description, cost, categories } = req.body;
+            const { userId } = req.headers;
+            const {filename} = req.file;
+
+            const user = await db.User.findById(userId)
+
+            if (!user) {
+                return res.status(422).json({ message: 'User Id does not exists' })
+            }
+
+            const event = await db.Event.create({
+                title,
+                description,
+                cost,
+                categories,
+                user: userId,
+                thumbnail: filename
+            })
+            return res.json(event);
+        } catch (error) {
+            throw Error(`Error while Creating  new Gradient :  ${error}`)
+        }
+    }
     
     }

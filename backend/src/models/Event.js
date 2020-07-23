bfc7ff4eb5bf85c045ffbf67c814034ea950d330
Ruma Das <rumaadl16@gmail.com>
2020-07-24 00:50:49 +0930

    adding emailtype in user model

--- backend/src/models/Event.js
+++ backend/src/models/Event.js
@@ -5,7 +5,6 @@ const EventSchema = new mongoose.Schema({
     description: String,
     price: Number,
     thumbnail: String
-
 })
-const Event = mongoose.model('Event', GradientSchema);
- module.exports = Event;
+const Event = mongoose.model('Event', EventSchema);
+module.exports = Event;
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,7 +4,15 @@ const UserSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     password: String,
-    email: String
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    }
 })
 const User = mongoose.model('User', UserSchema);
 module.exports = User;

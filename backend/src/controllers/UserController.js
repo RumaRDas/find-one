40e29bc3ccac645c6a75bd90e1ba7d978a55b13c
Ruma Das <rumaadl16@gmail.com>
2020-07-24 09:11:25 +0930

    adding approval controller

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -5,7 +5,6 @@ module.exports = {
 	async createUser(req, res) {
 		try {
 			const { email, firstName, lastName, password } = req.body;
-
 			const existentUser = await db.User.findOne({ email });
 			if (!existentUser) {
 				const hashedPassword = await bcrypt.hash(password, 10);
@@ -14,31 +13,26 @@ module.exports = {
 					lastName,
 					email,
 					password: hashedPassword,
-			
 				});
 				return res.json(user);
 			}
-
 			return res.status(400).json({
 				message: "email/user already exist! do you want to login?"
 			})
-
-
 		} catch (error) {
 			throw Error(`Error while Registering new user :  ${error}`)
 		}
-    },
+	},
 
-    async getUserById(req, res){
+	async getUserById(req, res) {
 		const { userId } = req.params;
-		try{
+		try {
 			const user = await db.User.findById(userId);
 			return res.json(user)
-		
-		}catch(error){
+		} catch (error) {
 			return res.status(422).json('User Id does not exists')
 		}
 
 
-}
+	}
 }

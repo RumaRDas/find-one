6b6961b29918a5457c7333d518845fb80198ffaa
Ruma Das <rumaadl16@gmail.com>
2020-07-24 09:20:41 +0930

    adding rejection controller

---
+++ backend/src/controllers/RejectionController.js
@@ -0,0 +1,16 @@
+const db = require('../models');
+
+module.exports = {
+    async approve(req, res) {
+        try {
+            const { registration_id } = req.params;
+            const registration = await db.Registration.findById(registration_id);
+            registration.approve = false;
+            await registration.save();
+            return res.json(registration)
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
+
+}

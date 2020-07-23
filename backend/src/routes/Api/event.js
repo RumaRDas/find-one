f73f06102ead38ccfd5f1efcf67e9dcd7cfa8533
Ruma Das <rumaadl16@gmail.com>
2020-07-24 02:41:39 +0930

    adding api and function for getting events

--- backend/src/routes/Api/event.js
+++ backend/src/routes/Api/event.js
@@ -6,13 +6,9 @@ const EventsController = require("../../controllers/EventsController");
 const uploadConfig = require('../../config/upload');
 const upload = multer(uploadConfig);
 
-router.get('/', (req, res) => {
-	res.send('Hello from event api app \n')
-})
-
 router.post('/',upload.single('thumbnail'), EventsController.createEvent)
 router.get('/:eventId', EventsController.getEventtById)
-router.get('/:categories', EventsController.getCateEvents)
+router.get('/categories/:categories', EventsController.getCateEvents)
 router.get('/', EventsController.getAllEvents)
 
 

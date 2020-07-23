cafadcd0b0e30404c273ab6023af0686b2880d2a
Ruma Das <rumaadl16@gmail.com>
2020-07-24 03:03:34 +0930

    adding login api

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,12 @@
 const path = require("path");
 const  router  = require("express").Router();
 const userRoutes = require("./Api/users");
-const eventRoutes =require('./Api/event')
+const eventRoutes = require('./Api/event');
+const loginRoutes = require('./Api/login');
+const dashboardRoutes = require('./Api/')
 
+
+router.use("/api/login", loginRoutes);
 router.use("/api/users", userRoutes);
 router.use("/api/event", eventRoutes);
 

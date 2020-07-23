e24f8488c2e793194508914a7c5a603ffe6dad21
Ruma Das <rumaadl16@gmail.com>
2020-07-24 03:46:45 +0930

    adding registration api

--- backend/src/controllers/RegistrationController.js
+++ backend/src/controllers/RegistrationController.js
@@ -5,12 +5,12 @@ module.exports ={
 
     async create(req, res) {
         const { user_id } = req.headers;
-        const { gradientid } = req.headers;
+        const {eventId } = req.headers;
         const { date } = req.body;
 
         const registration = await db.Registration.create({
             user: user_id,
-            gradient: gradientid,
+            event: eventId,
             date
         })
         return res.json(registration);

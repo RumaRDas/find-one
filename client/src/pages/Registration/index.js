b1dc0af1ca16767d63b2e63f1728071100e6f2ca
Ruma Das <rumaadl16@gmail.com>
2020-07-24 13:46:47 +0930

    adding registration component

--- client/src/pages/Registration/index.js
+++ client/src/pages/Registration/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import API from '../../services/API'
 import './style.css';
 
 function Registration() {
     return (
         <div>
-            <h1>Hello from Registration</h1>
+            <div className="field" >
+                <div className="control">
+                    <input className="input is-danger" type="text" placeholder="First Name" name="firstName" id="firstName"  />
+                </div>
+            </div>
+            <div className="field" >
+                <div className="control">
+                    <input className="input is-success" type="text" placeholder="Last Name" name="lastName" id="lastName"/>
+                </div>
+            </div>
+            <div className="field" >
+                <div className="control">
+                    <input className="input is-danger" type="email" placeholder="Your email" name="email" id="email"  />
+                </div>
+            </div>
+            <div className="field">
+                <div className="control">
+                    <input className="input is-info" type="password" name="password" id="password" placeholder="Password"  />
+                </div>
+            </div>
+            <div className="control">
+                <button className="button is-link" >Submit</button>
+            </div>
         </div>
     )
 }

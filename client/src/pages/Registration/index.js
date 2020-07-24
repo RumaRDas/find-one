7a9f43b61037da1b477aea402f1b1bfa9f393747
Ruma Das <rumaadl16@gmail.com>
2020-07-24 13:57:08 +0930

    addign funcility in Registration page

--- client/src/pages/Registration/index.js
+++ client/src/pages/Registration/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import API from '../../services/API'
 import './style.css';
 
@@ -9,30 +9,45 @@ function Registration() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const handleSubmit = async evt => {
+        evt.preventDefault();
+        console.log("result of submit", email, password, firstName, lastName);
+
+        const response = await api.post('/api/registration', { firstName, lastName, email, password })
+        const userId = response.data._id || false;
+        if (userId) {
+            localStorage.setItem('user', userId)
+            history.push('/dashboard')
+        } else {
+            const { message } = response.data
+            console.log(message)
+        }
+    }
+
     return (
         <div>
             <div className="field" >
                 <div className="control">
-                    <input className="input is-danger" type="text" placeholder="First Name" name="firstName" id="firstName"  />
+                    <input className="input is-danger" type="text" placeholder="First Name" name="firstName" id="firstName" onChange={evt => setFirstName(evt.target.value)} />
                 </div>
             </div>
             <div className="field" >
                 <div className="control">
-                    <input className="input is-success" type="text" placeholder="Last Name" name="lastName" id="lastName"/>
+                    <input className="input is-success" type="text" placeholder="Last Name" name="lastName" id="lastName" onChange={evt => setLastName(evt.target.value)} />
                 </div>
             </div>
             <div className="field" >
                 <div className="control">
-                    <input className="input is-danger" type="email" placeholder="Your email" name="email" id="email"  />
+                    <input className="input is-danger" type="email" placeholder="Your email" name="email" id="email" onChange={evt => setEmail(evt.target.value)} />
                 </div>
             </div>
             <div className="field">
                 <div className="control">
-                    <input className="input is-info" type="password" name="password" id="password" placeholder="Password"  />
+                    <input className="input is-info" type="password" name="password" id="password" placeholder="Password" onChange={evt => setPassword(evt.target.value)} />
                 </div>
             </div>
             <div className="control">
-                <button className="button is-link" >Submit</button>
+                <button className="button is-link" onClick={handleSubmit}>Submit</button>
             </div>
         </div>
     )

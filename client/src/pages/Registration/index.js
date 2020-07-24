53ef2714c16949614dfed9d8343503c29e8e1854
Ruma Das <rumaadl16@gmail.com>
2020-07-24 13:47:57 +0930

    adding useState

--- client/src/pages/Registration/index.js
+++ client/src/pages/Registration/index.js
@@ -3,6 +3,12 @@ import API from '../../services/API'
 import './style.css';
 
 function Registration() {
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <div>
             <div className="field" >

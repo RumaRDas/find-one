3841a7d70d57b80d66ce9e1637c3d2f88f7dc599
Ruma Das <rumaadl16@gmail.com>
2020-07-24 12:32:42 +0930

    adding ligin return

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -4,8 +4,25 @@ import DashBoard from '../Dashboard'
 const index = () =>{
     return (
         <div>
-            <h1>Hello Login</h1>
-            <DashBoard/>
+            <div className="field" >
+                <div className="control has-icons-left has-icons-right">
+                    <input className="input is-danger" type="email" placeholder="Your email" name ="email" id= "email" />
+                    <span className="icon is-small is-left">
+                        <i className="fas fa-envelope"></i>
+                    </span>
+                    <span className="icon is-small is-right">
+                        <i className="fas fa-exclamation-triangle"></i>
+                    </span>
+                </div>
+            </div>
+            <div className="field">
+                <div className="control">
+                    <input className="input" type="password" name="password" id="password" placeholder="Password" />
+                </div>
+            </div>
+            <div className="control">
+                <button className="button is-link">Submit</button>
+            </div>
         </div>
     )
 }

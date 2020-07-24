0cca3b15021672f7358b1a23782112e4ee06ac74
Ruma Das <rumaadl16@gmail.com>
2020-07-24 12:56:40 +0930

    adding funcility in login

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from '../../components/Container';
 import API from '../../services/API'
 import './style.css'
 
-const index = ({ histrory }) => {
+const Login= ({history}) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -11,7 +11,7 @@ const index = ({ histrory }) => {
     const handleSubmit = async evt => {
         evt.preventDefault();
         console.log("result of submit", email, password);
-        const response = await api.post('/login', { email, password })
+        const response = await API.post('/api/login', { email, password })
         const userId = response.data._id || false;
         if (userId) {
             localStorage.setItem('user', userId)
@@ -26,28 +26,30 @@ const index = ({ histrory }) => {
     return (
         <Container>
         <div>
-        <div className="field" >
-            <div className="control has-icons-left has-icons-right">
-                <input className="input is-danger" type="email" placeholder="Your email" name ="email" id= "email" onChange={evt =>setEmail(evt.target.value)} />
-                <span className="icon is-small is-left">
-                    <i className="fas fa-envelope"></i>
-                </span>
-                <span className="icon is-small is-right">
-                    <i className="fas fa-exclamation-triangle"></i>
-                </span>
-            </div>y
-        </div>
-        <div className="field">
+            <div className="field" >
+                <div className="control has-icons-left has-icons-right">
+                    <input className="input is-danger" type="email" placeholder="Your email" name ="email" id= "email" onChange={evt =>setEmail(evt.target.value)} />
+                    <span className="icon is-small is-left">
+                        <i className="fas fa-envelope"></i>
+                    </span>
+                    <span className="icon is-small is-right">
+                        <i className="fas fa-exclamation-triangle"></i>
+                    </span>
+                </div>y
+            </div>
+            <div className="field">
+                <div className="control">
+                    <input className="input" type="password" name="password" id="password" placeholder="Password" onChange={evt =>setPassword(evt.target.value)}  />
+                </div>
+            </div>
             <div className="control">
-                <input className="input" type="password" name="password" id="password" placeholder="Password" onChange={evt =>setPassword(evt.target.value)}  />
+                <button className="button is-link" onClick={handleSubmit}>Submit</button>
             </div>
         </div>
-        <div className="control">
-            <button className="button is-link" onClick={handleSubmit}>Submit</button>
-        </div>
-    </div>
+    )
+}
         </Container>
     )
 }
 
-export default index;
+export default Login;

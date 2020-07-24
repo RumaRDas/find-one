3e5b11beb554f389032f31508c09883668351aed
Ruma Das <rumaadl16@gmail.com>
2020-07-25 02:51:15 +0930

    adding event[ages

--- client/src/pages/EventPages/index.js
+++ client/src/pages/EventPages/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Container from '../../components/Container'
+import API from '../../services/API'
+import moment from 'moment';
 import './style.css';
 
 const EventPages = () => {

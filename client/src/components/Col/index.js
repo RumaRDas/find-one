403ef97fb566994da5b9f9590def6eb4b4e36713
Ruma Das <rumaadl16@gmail.com>
2020-07-24 11:42:23 +0930

    adding pages and componemts in client side

---
+++ client/src/components/Col/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index(props) {
+    return (
+        <div className="columns is-mobile is-centered">
+            <div>{props}</div>
+        </div>
+    )
+}
+
+export default index;

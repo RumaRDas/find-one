0673e19f662d5177ba0a42a05524d433534e4dc3
Ruma Das <rumaadl16@gmail.com>
2020-07-25 02:55:41 +0930

    adding rvent pages

--- client/src/pages/EventPages/index.js
+++ client/src/pages/EventPages/index.js
@@ -1,15 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import Container from '../../components/Container'
-import API from '../../services/API'
-import moment from 'moment';
 import './style.css';
 
 const EventPages = () => {
     return (
-        <div>
-         <h1> Hello from Event Pages</h1>   
-        </div>
-    )
-}
+
+        <>
+          <div className="filter-panel">
+            <div className="buttons field has-addons ">
+              <button className="button is-warning"  >ALL Color</button>
+              <button className="button is-danger" >My Gradieny</button>
+              <button className="button is-success" >Red</button>
+              <button className="button is-info" >Blue</button>
+              <button className="button is-danger" >Black</button>
+            </div>
+            <button className="button is-dark"  >Back</button>
+          </div>
+          <div>
+            <ul className="gradient-list">
+           <li>
+                    <header className="deletBtn">                
+                          <div>
+                            <button className="button is-danger is small">Delete</button>
+                          </div>
+                    </header>
+                    <strong>{gradient.title}</strong>
+                    <span>Gradient Date: </span>
+                    <span> Gradient Top:</span>
+                    <span> gradient price:</span>
+                    <span></span>
+                    <button className="button is-small is-dark ">Subscribe</button>
+   
+                  </li>
+
+            </ul>
+                   </div>
+        </>
+    
+      )
+    }
+    
+
 
 export default EventPages;

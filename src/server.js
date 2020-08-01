6f70573b25fcd0f6a906de41b3d9b57eff23c257
Ruma Das <rumaadl16@gmail.com>
2020-08-01 15:38:32 +0930

    commit

--- src/server.js
+++ src/server.js
@@ -14,9 +14,10 @@ app.use(express.json())
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 }
-// let MONGO_DB_CONNECTION ="mongodb+srv://temp_user:tAkmNN8jC3cPXqBh@cluster0.krzns.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
+let MONGO_DB_CONNECTION ="mongodb+srv://temp_user:tAkmNN8jC3cPXqBh@cluster0.krzns.mongodb.net/<dbname>?retryWrites=true&w=majority"
 try {
-	mongoose.connect(process.env.MONGO_DB_CONNECTION || "mongodb://localhost/ mongoHeadlines" , {
+	mongoose.connect(MONGO_DB_CONNECTION || "mongodb://localhost/ mongoHeadlines" , {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
@@ -29,10 +30,10 @@ app.use("/files", express.static(path.resolve(__dirname, "..", "files")))
 
 app.use(routes);
 
-app.use(express.static(path.resolve(__dirname, "../../", "client/build")))
+app.use(express.static(path.resolve(__dirname, "../client/build")))
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../../client/build/index.html'));
+	res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`)

902e5db387326c9141b85ecc033960fbcc35691a
Ruma Das <rumaadl16@gmail.com>
2020-08-01 14:53:06 +0930

    adding heroku

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const routes = require('./routes');
 const path = require("path");
 
+
 const app = express()
 const PORT = process.env.PORT || 4000
 
@@ -13,7 +14,7 @@ app.use(express.json())
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 }
-
+let MONGO_DB_CONNECTION ="mongodb+srv://temp_user:tAkmNN8jC3cPXqBh@cluster0.krzns.mongodb.net/<dbname>?retryWrites=true&w=majority"
 try {
 	mongoose.connect(process.env.MONGO_DB_CONNECTION || "mongodb://localhost/ mongoHeadlines" , {
 		useNewUrlParser: true,

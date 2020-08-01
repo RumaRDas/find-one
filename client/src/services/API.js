050888a4142ac318e7d5ff3a52643ff3a8c5f9f5
Ruma Das <rumaadl16@gmail.com>
2020-08-01 13:29:51 +0930

    adding Heroke

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: `http://localhost:4000`
+    baseURL: process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:4000'
 }
 )
 export default API;
